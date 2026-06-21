56567092bfd97dd30f69ea2777c78d853fb76206
tom <opaquu@gmail.com>
2026-06-21 15:57:27 +0900

    Initial Commit

---
+++ Island Now/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Island Now
+//
+//  Created by TOMOYUKI SHIDARA on 2026/06/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Island Now/Island_NowApp.swift
@@ -0,0 +1,17 @@
+//
+//  Island_NowApp.swift
+//  Island Now
+//
+//  Created by TOMOYUKI SHIDARA on 2026/06/21.
+//
+
+import SwiftUI
+
+@main
+struct Island_NowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
